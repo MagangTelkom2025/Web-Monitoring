de9fa5c714a9dc7acf95328400dff5007f6322c5
faqihhidayatullah <faqihhidayatullah0@gmail.com>
2025-08-11 10:58:00 +0700

    update: auth routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,8 +11,13 @@ $routes->get('/login', 'Auth::login');
 $routes->post('/auth/authenticate', 'Auth::authenticate');
 $routes->get('/auth/forgot-password', 'Auth::forgotPassword');
 $routes->get('/logout', 'Auth::logout');
-$routes->get('/absen', 'Absen::index');
+//$routes->get('/absen', 'Absen::index');
 
-$routes->get('ticket', 'Tickets::index');           // List view
-$routes->get('tickets/upload', 'Tickets::uploadForm'); // Form upload
+//$routes->get('ticket', 'Tickets::index');           // List view
+
+$routes->group('', ['filter' => 'auth'], function($routes) {
+    $routes->get('/ticket', 'Tickets::index');
+    $routes->get('/absen', 'Absen::index');
+    $routes->get('tickets/upload', 'Tickets::uploadForm'); // Form upload
 $routes->post('tickets/upload', 'Tickets::upload');    // Proses upload
+});
